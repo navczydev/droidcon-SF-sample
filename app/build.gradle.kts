884c70b3bb2a4db419a1e3d4acb771f2be5dab62
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-05-29 21:47:17 -0400

    Android 13 specific back gesture implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.navgde.droidconsf"
-//    compileSdk = 33
-    compileSdkPreview = "UpsideDownCake"
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.navgde.droidconsf"
         minSdk = 26
-//        targetSdk = 33
-        targetSdkPreview = "UpsideDownCake"
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -46,10 +44,10 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("com.google.android.material:material:1.10.0-alpha03")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("androidx.activity:activity-ktx:1.8.0-alpha04")
+    implementation("androidx.activity:activity-ktx:1.7.2")
 
     // compose
     val composeBom = platform("androidx.compose:compose-bom:2023.04.01")
@@ -60,7 +58,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     // Optional - Integration with activities
     // 1.8.0-alpha01 track state of back
-    implementation("androidx.activity:activity-compose:1.8.0-alpha04")
+    implementation("androidx.activity:activity-compose:1.7.2")
     // Optional - Integration with ViewModels
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     // Android Studio Preview support
--- app/src/main/java/com/navgde/droidconsf/activities/SecondActivity.kt
+++ app/src/main/java/com/navgde/droidconsf/activities/SecondActivity.kt
@@ -1,10 +1,6 @@
 package com.navgde.droidconsf.activities
 
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
-import android.window.BackEvent
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -38,54 +34,6 @@ class SecondActivity : AppCompatActivity() {
                 SanFranciscoImage()
             }
         }
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            Log.d(TAG, "onCreate: Android14")
-            overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, R.anim.slide_in_left, R.anim.slide_out_left)
-            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, R.anim.slide_in_left, R.anim.slide_out_left)
-
-
-            val composeHolder = binding.cvHolder
-            val screenWidth = Resources.getSystem().displayMetrics.widthPixels
-            val maxXShift = (screenWidth / 20)
-
-
-            // For the purposes of demonstration, this code snippet presents the
-            // callback as being always enabled. However, in practice it's always a
-            // good idea to always disable the callback when you're ready for the
-            // system to handle back events.
-            // Progress callbacks available since :-
-            // https://developer.android.com/jetpack/androidx/releases/activity#1.8.0-alpha01
-            val callback = object : OnBackPressedCallback(enabled = true) {
-
-                override fun handleOnBackProgressed(backEvent: BackEvent) {
-                    Log.d(TAG, "handleOnBackProgressed: Back gesture progressing")
-                    when (backEvent.swipeEdge) {
-                        BackEvent.EDGE_LEFT ->
-                            composeHolder.translationX = backEvent.progress * maxXShift
-
-                        BackEvent.EDGE_RIGHT ->
-                            composeHolder.translationX = -(backEvent.progress * maxXShift)
-                    }
-                    composeHolder.scaleX = 1F - (0.1F * backEvent.progress)
-                    composeHolder.scaleY = 1F - (0.1F * backEvent.progress)
-                }
-
-                override fun handleOnBackPressed() {
-                    // Do something after the back gesture completes.
-                    Log.d(TAG, "handleOnBackPressed: Back gesture completed")
-                }
-
-                override fun handleOnBackCancelled() {
-                    // e.g. reset box to the original position
-                    Log.d(TAG, "handleOnBackCancelled: Back gesture cancelled")
-                    this.remove()
-
-                }
-            }
-            this.onBackPressedDispatcher.addCallback(callback)
-        //}
-
-
     }
 
     companion object {
