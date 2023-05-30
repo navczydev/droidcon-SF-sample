599abf7b50e81b870e3daa839cda191adb2b74b1
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-05-29 21:27:33 -0400

    cleanup

--- app/src/main/java/com/navgde/droidconsf/activities/SecondActivity.kt
+++ app/src/main/java/com/navgde/droidconsf/activities/SecondActivity.kt
@@ -7,21 +7,17 @@ import android.window.BackEvent
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.navgde.droidconsf.R
 import com.navgde.droidconsf.databinding.ActivitySecondBinding
 
@@ -42,54 +38,57 @@ class SecondActivity : AppCompatActivity() {
                 SanFranciscoImage()
             }
         }
-        overrideActivityTransition(
-            OVERRIDE_TRANSITION_CLOSE,
-            R.anim.slide_in_left,
-            R.anim.slide_out_left
-        )
-
-        val composeHolder = binding.cvHolder
-        val screenWidth = Resources.getSystem().displayMetrics.widthPixels
-        val maxXShift = (screenWidth / 20)
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            Log.d(TAG, "onCreate: Android14")
+            overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, R.anim.slide_in_left, R.anim.slide_out_left)
+            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, R.anim.slide_in_left, R.anim.slide_out_left)
 
 
-        // For the purposes of demonstration, this code snippet presents the
-        // callback as being always enabled. However, in practice it's always a
-        // good idea to always disable the callback when you're ready for the
-        // system to handle back events.
-        val callback = object : OnBackPressedCallback(enabled = true) {
+            val composeHolder = binding.cvHolder
+            val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+            val maxXShift = (screenWidth / 20)
 
-            override fun handleOnBackProgressed(backEvent: BackEvent) {
-                Log.d(TAG, "handleOnBackProgressed: Back gesture progressing")
-                when (backEvent.swipeEdge) {
-                    BackEvent.EDGE_LEFT ->
-                        composeHolder.translationX = backEvent.progress * maxXShift
 
-                    BackEvent.EDGE_RIGHT ->
-                        composeHolder.translationX = -(backEvent.progress * maxXShift)
+            // For the purposes of demonstration, this code snippet presents the
+            // callback as being always enabled. However, in practice it's always a
+            // good idea to always disable the callback when you're ready for the
+            // system to handle back events.
+            // Progress callbacks available since :-
+            // https://developer.android.com/jetpack/androidx/releases/activity#1.8.0-alpha01
+            val callback = object : OnBackPressedCallback(enabled = true) {
+
+                override fun handleOnBackProgressed(backEvent: BackEvent) {
+                    Log.d(TAG, "handleOnBackProgressed: Back gesture progressing")
+                    when (backEvent.swipeEdge) {
+                        BackEvent.EDGE_LEFT ->
+                            composeHolder.translationX = backEvent.progress * maxXShift
+
+                        BackEvent.EDGE_RIGHT ->
+                            composeHolder.translationX = -(backEvent.progress * maxXShift)
+                    }
+                    composeHolder.scaleX = 1F - (0.1F * backEvent.progress)
+                    composeHolder.scaleY = 1F - (0.1F * backEvent.progress)
                 }
-                composeHolder.scaleX = 1F - (0.1F * backEvent.progress)
-                composeHolder.scaleY = 1F - (0.1F * backEvent.progress)
-            }
 
-            override fun handleOnBackPressed() {
-                // Do something after the back gesture completes.
-                Log.d(TAG, "handleOnBackPressed: Back gesture completed")
-            }
+                override fun handleOnBackPressed() {
+                    // Do something after the back gesture completes.
+                    Log.d(TAG, "handleOnBackPressed: Back gesture completed")
+                }
 
-            override fun handleOnBackCancelled() {
-                // e.g. reset box to the original position
-                Log.d(TAG, "handleOnBackCancelled: Back gesture cancelled")
-                this.remove()
+                override fun handleOnBackCancelled() {
+                    // e.g. reset box to the original position
+                    Log.d(TAG, "handleOnBackCancelled: Back gesture cancelled")
+                    this.remove()
 
+                }
             }
-        }
-        this.onBackPressedDispatcher.addCallback(callback)
+            this.onBackPressedDispatcher.addCallback(callback)
+        //}
 
 
     }
 
-    companion object{
+    companion object {
         private const val TAG = "SecondActivity"
     }
 }
@@ -98,7 +97,7 @@ class SecondActivity : AppCompatActivity() {
 fun SanFranciscoImage() {
     MaterialTheme {
         Column {
-            Text("Droidcon-23")
+            Text("DroidConSF-23")
             Image(
                 painter = painterResource(id = R.drawable.san_francisco),
                 contentDescription = "Golden bridge",
