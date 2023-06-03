c9028bc2c43e837251f312e4829591943a2b9432
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-06-03 17:25:33 -0400

    lifecycle-runtime and androidX transition deps added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,11 +46,10 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("com.google.android.material:material:1.10.0-alpha03")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
     implementation("androidx.activity:activity-ktx:1.8.0-alpha04")
-
     // compose
     val composeBom = platform("androidx.compose:compose-bom:2023.04.01")
     implementation(composeBom)
@@ -66,7 +65,7 @@ dependencies {
     // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
+    implementation("androidx.transition:transition-ktx:1.5.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
