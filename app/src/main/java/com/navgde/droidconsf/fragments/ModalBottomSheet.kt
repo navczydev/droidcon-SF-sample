43f7a690e8d3183b360404dee120cbf514f4ca26
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-06-03 17:27:24 -0400

    code for adding backPressedCallback to activity from BottomSheet added

--- app/src/main/java/com/navgde/droidconsf/fragments/ModalBottomSheet.kt
+++ app/src/main/java/com/navgde/droidconsf/fragments/ModalBottomSheet.kt
@@ -4,21 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.navgde.droidconsf.R
+import com.navgde.droidconsf.composables.BottomSheetContent
+import com.navgde.droidconsf.databinding.ModalBottomSheetContentBinding
 
 /**
  * @author Nav Singh
  */
 class ModalBottomSheet : BottomSheetDialogFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.modal_bottom_sheet_content, container, false)
+    private lateinit var binding: ModalBottomSheetContentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = ModalBottomSheetContentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cvHolder.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                BottomSheetContent()
+            }
+        }
+        // custom back handling in fragment
+        /*     requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+                 override fun handleOnBackPressed() {
+                     TODO("Not yet implemented")
+
+                 }
+
+             })
+
+             requireActivity().onBackPressedDispatcher.addCallback {
+
+             }*/
+    }
 
     companion object {
         const val TAG = "ModalBottomSheet"
     }
 }
+
